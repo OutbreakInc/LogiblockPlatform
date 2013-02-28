3044199c7830028b122d99ef70e13e7101309de9
Kuy Mainwaring <kuy@outbreak.co>
2013-02-28 02:56:40 -0800

    make platform json compliant and improve the prepare filter too

--- prepare-module.sh
+++ prepare-module.sh
@@ -2,4 +2,4 @@
 
 # from the IDE root directory, find all files, remove those on the specified blacklist and use cpio (rather than tar) to create a clean tar file to pipe through gzip.
 #   the resulting tar.gz can be uploaded to moduleverse or otherwise distributed.
-find . | grep -v -E "/\.|\./prepare-module\.sh" | cpio -o --format ustar | gzip -8 > ../platform.release.tar.gz
+find . | grep -v -E "/\.|/examples|/test|/tests|\./prepare-module\.sh" | cpio -o --format ustar | gzip -8 > ../platform.release.tar.gz
