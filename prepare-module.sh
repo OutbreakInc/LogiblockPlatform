b81895472013744f5aa1c7e9aab7b63649eb0b7c
Kuy Mainwaring <kuy@outbreak.co>
2013-05-21 16:27:24 -0700

    bumped platform to 0.7 and improved the platform module prepare script

--- prepare-module.sh
+++ prepare-module.sh
@@ -3,3 +3,7 @@
 # from the IDE root directory, find all files, remove those on the specified blacklist and use cpio (rather than tar) to create a clean tar file to pipe through gzip.
 #   the resulting tar.gz can be uploaded to moduleverse or otherwise distributed.
 find . | grep -v -E "/\.|/examples|/test|/tests|\./prepare-module\.sh" | cpio -o --format ustar | gzip -8 > ../platform.release.tar.gz
+
+echo "Uploading module..."
+curl -v --user logiblock -X PUT --upload-file ../platform.release.tar.gz http://moduleverse.com/v1/modules
+echo ""
